1d197ea7ca25f8b4e7ae370b05183822fe6ea25e
Greatest Lord <greatestlords@gmail.com>
2023-06-16 09:28:38 +0530

    fixed: put `simplebar.css` file content into scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 require('bootstrap')
-import 'simplebar' // or "import SimpleBar from 'simplebar';" if you want to use it manually.
-// import 'simplebar/dist/simplebar.css'
-
-// You will need a ResizeObserver polyfill for browsers that don't support it! (iOS Safari, Edge, ...)
+import 'simplebar'
 import ResizeObserver from 'resize-observer-polyfill'
 window.ResizeObserver = ResizeObserver
 
